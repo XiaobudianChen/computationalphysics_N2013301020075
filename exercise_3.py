a061f54e64460616c0bc8b0a60b3f2a9c6d1f51d
Zheng Chen <chenzhsxbd@163.com>
2016-03-15 22:43:56 +0800

    Update exercise_3.py

--- exercise_3.py
+++ exercise_3.py
@@ -105,3 +105,6 @@ G = ['   @@@@@@@@@@@  ',
      '  @@@@@@@@@@@@  ']
 for i in range(12):
      print C[i], H[i],E[i],N[i],Z[i],H[i],E[i],N[i],G[i]
+
+运行结果如下：
+![](https://github.com/XiaobudianChen/computationalphysics_N2013301020075/blob/master/%E2%80%9CCHENZHENG%E2%80%9D.png)
