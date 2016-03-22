e2252a81a9d13d5f11a919610b4cbd3390e1dad1
Zheng Chen <chenzhsxbd@163.com>
2016-03-23 07:37:29 +0800

    Added files via upload

---
+++ chapter1/1.1.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a script file for exercise 1.1 in Chapter 1.
+"""
+#python
+v = []
+t = []
+g = 9.8
+dt = float(input('time step='))
+v.append(0)
+t.append(0)
+end_time=10
+f=open('chapter one 1.1.txt','w')
+for i in range(int(end_time/dt)):
+    v.append(v[i]-g*dt)
+    t.append((i+1)*dt)
+    print t[-1],v[-1]
+    print >> f,t[-1],v[-1]
+f.close() 
+   
+plot(t,v)
+legend(('t','v'))
+title('problem 1.1',fontsize=20)
+xlabel('t/s')
+ylabel('v')
+savefig('chapter one 1.1.png')
+show()
