5350e040120f597270bd48b6794e34dfe2422e42
Zheng Chen <chenzhsxbd@163.com>
2016-06-21 21:37:22 +0800

    Add files via upload

--- Final-Paper/Buffon.py
+++ Final-Paper/Buffon.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+1# -*- coding: utf-8 -*-
 """
 Created on Tue Jun 21 2016
 
@@ -6,6 +6,7 @@ Created on Tue Jun 21 2016
 """
 
 import random
+import math
 
 a = input ("a=")
 l = input ("l=")
@@ -14,12 +15,12 @@ step = input("step=")
 m = 0
 if a > 0 and l < a:
     for i in range(step):
-        a = random.randint(0,60)
-        b = random.randint(0,60)
-    if abs(a-b)<=10:
-        m += 1
-    else:
-        m = m
+        x = random.random()*0.5*a
+        phi = random.random()*math.pi
+        if x <= 0.5*l*math.sin(phi):
+            m += 1
+        else:
+            m = m    
     print float(m)/step
 else:
    print "You have input the wrong number!Please input a>0,l<a"
